9f865822fc5a01ca4de3d3500b31863abc7680df
Misbah Imtiaz <misbahaving@wireless-10-145-82-40.public.utexas.edu>
2022-03-25 23:44:01 -0500

    facial recognition complete, able to detect my face

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import cv2
-import face_recognition as fr
+import face_recognition as face_rec
 import numpy as np
 import os
 import glob
 
-# i installed these libraries
-# pip install cmake face_recognition numpy opencv-python
-
-webcam = cv2.VideoCapture(0)
-
-
+# takes an image and returns the image encoding
+# MATH Explained:
 def encode_image(path):
     img = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
-    img_encoding = fr.face_encodings(img)[0]
+    img_encoding = face_rec.face_encodings(img)[0]
     return img_encoding
 
 
-# result = face_recognition.compare_faces([encode_image("faces/nabil.jpg")], encode_image("Misbah.jpg"))
-# print("Result: ", result)
+def draw_rectangle(face_location, this_frame, name):
+    y1, x1, y2, x2 = face_location[0], face_location[1], face_location[2], face_location[3]
+    cv2.rectangle(this_frame, (x2, y1), (x1, y2), (0, 0, 255), 2)
+    cv2.rectangle(this_frame, (x2, y2 - 35), (x1, y2), (0, 0, 255), cv2.FILLED)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(this_frame, name, (x2 + 6, y2 - 6), font, 1.0, (255, 255, 255), 1)
+    return this_frame
 
+
+webcam = cv2.VideoCapture(0)
 curPath = os.path.join(os.getcwd(), "faces/")
 registered_photos = glob.glob(curPath + '*.jpg')
 num_of_photos = len(registered_photos)
@@ -30,25 +33,30 @@ for photos in registered_photos:
     names.append(str(photos).replace(curPath, "").replace('.jpg', ""))
     registered_encodings.append(encode_image(photos))
 
-# # code from Behic Guven
-# for i in range(num_of_photos):
-#     globals()['image_{}'.format(i)] = face_recognition.load_image_file(registered_photos[i])
-#     globals()['image_encoding_{}'.format(i)] = face_recognition.face_encodings(globals()['image_{}'.format(i)])[0]
-#     faces_encodings.append(globals()['image_encoding_{}'.format(i)])
 
 while True:
     # Display current frame
     ret, frame = webcam.read()
-    cv2.imshow("frame:", frame)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # encode all faces on the frame (could be more than one face detected)
-    face_encodings = fr.face_encodings(frame, fr.face_locations(frame))
+    # locate and encode all faces on the frame (could be more than one face detected)
+    face_locations = face_rec.face_locations(frame)
+    face_encodings = face_rec.face_encodings(frame, face_locations)
 
-    # press q to exit
+    # take current encoding and face location on frame, compare it to all known encodings
+    for cur_face_location, cur_face_encoding in zip(face_locations, face_encodings):
+        face_distance = face_rec.face_distance(registered_encodings, cur_face_encoding)
+        matches = face_rec.compare_faces(registered_encodings, cur_face_encoding)
+        identified = "Unknown"
+        best_match_index = np.argmin(face_distance)
+        if matches[best_match_index]:
+            identified = names[best_match_index]
+        frame = draw_rectangle(cur_face_location, frame, identified)
+    cv2.imshow('Webcam_face_recognition', frame)
+
+    # press q to exit, later integrate this functionality with an app
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 webcam.release()
 cv2.destroyAllWindows()
-
-# encode the image in order to compare it to other images
