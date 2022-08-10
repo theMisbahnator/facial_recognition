598e90ef7fe0723e5d3e9f47633387c8b0dbc27e
Misbah Imtiaz <misbahaving@Misbahs-MacBook-Pro.local>
2022-08-10 01:48:15 -0500

    added model component and basic structure for a form to add user

--- frontend/src/components/AddUser.js
+++ frontend/src/components/AddUser.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FcPlus } from "react-icons/fc";
-import '../index'
+import { GrClose } from "react-icons/gr";
 
+import '../index'
+import Modal from 'react-modal';
+
+Modal.setAppElement("#root"); 
 const AddUser = () => {
+    const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-        <FcPlus className="adduser" size={50}/>
+        <FcPlus className="adduser" size={50} onClick={()=>{setIsOpen(true)}}/>
+
+        <Modal className="add-modal" isOpen={isOpen} onRequestClose={() => {setIsOpen(false)}}>
+            <div className='content-form'>
+                <div className='close-form'>
+                    <button className="close-button" onClick={() => {setIsOpen(false)}}><GrClose/></button>
+                </div>
+                <div className='display-4 title-form'>
+                    Add User
+                </div>
+
+                <div className='form-elem'>
+                    <input className ="form-control" type="text" placeholder='Enter a name...'></input>
+                </div>
+                
+                <div className='form-elem'>
+                    <input className ="form-control" type="text" placeholder='Paste link from Youtube...'></input>
+                </div>
+               
+                <div className='form-elem'>
+                    <input className ="form-control" type="file" placeholder='Upload a photo of yourself...'></input>
+                </div>
+                
+                <div className='submit-form'>
+                    <button type="button" class="btn btn-success">Create!</button>
+                </div>
+                
+
+            </div>
+        </Modal>
+        
+        
     </div>
   )
 }
--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,7 +1,7 @@
 import axios from "axios"; 
 import React, { useState } from 'react'
 import "../index"
-import { MdDelete } from "react-icons/md";
+import { FcFullTrash } from "react-icons/fc";
 
 const User = ({id, name, song_title, yt_url, img_url, last_updated, date_created}) => {
     const [url, setUrl] = useState("");
@@ -20,14 +20,14 @@ const User = ({id, name, song_title, yt_url, img_url, last_updated, date_created
                         <p className="container">
                             <div className="row justify-content-around">
                                 <button type="button" class="btn btn-warning col-3">Edit 📝</button>
-                                <button type="button" class="btn btn-danger col-3">Delete ✂️</button>
+                                <button type="button" class="btn btn-danger col-3">Delete <FcFullTrash/></button>
                             </div>
                         </p>
                         <div class="card creation-details">
                         <p className="container">
-                            <div className="row">
-                                <small class="text-muted col">Last updated: {last_updated}</small>
-                                <small class="text-muted col">Date Created: {date_created}</small>
+                            <div className="row date-holder">
+                                <small class="text-muted col dates">Last updated: {last_updated}</small>
+                                <small class="text-muted col dates">Date Created: {date_created}</small>
                             </div>
                         </p>
                     </div>
